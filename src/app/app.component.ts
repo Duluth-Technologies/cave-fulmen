4fadbbddf3f846408fd53b9a69a7062e34729dfb
Vincent Mijoule <vincent.mijoule@duluthtechnologies.com>
2024-08-16 23:54:17 +0200

    Fix flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CAMERA_SERVICE_TOKEN } from './services/camera.service';
 import { CameraServiceImpl } from './services/impl/camera-impl.service';
-import { distinctUntilChanged, exhaustMap, interval, map, Observable, switchMap } from 'rxjs';
+import { concatMap, distinctUntilChanged, exhaustMap, map, switchMap } from 'rxjs/operators';
 import { Camera } from './model/camera.model';
 import { OSM_SERVICE_TOKEN } from './services/osm.service';
 import { OsmServiceLocalImpl } from './services/impl/osm-local-impl.service';
-import {provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { interval, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,27 +21,38 @@ import { CommonModule } from '@angular/common';
     { provide: OSM_SERVICE_TOKEN, useClass: OsmServiceLocalImpl }
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'cavefulmen';
   speedLimitThreshold = 4;
-  cameraInfo$!: Observable<{maxSpeed: number, distance: number}>;
+  cameraInfo$: Observable<{ maxSpeed: number, distance: number } | null> = of(null) ;
+  lat: number | null = 0;
+  lon: number | null = 0;
+  watchId: number | null = null;
 
 
-  constructor(@Inject(CAMERA_SERVICE_TOKEN) private cameraService: CameraServiceImpl) {    
+  constructor(@Inject(CAMERA_SERVICE_TOKEN) private cameraService: CameraServiceImpl) {
   }
 
   ngOnInit(): void {
-    this.cameraInfo$ = this.getCurrentPosition().pipe(
-      distinctUntilChanged((prev, curr) => prev.latitude === curr.latitude && prev.longitude === curr.longitude),
-      exhaustMap(coords => 
-        this.cameraService.getClosestCamera(coords.latitude, coords.longitude).pipe(
+    this.watchPosition();
+    this.cameraInfo$ = interval(5000).pipe(
+      concatMap(() => {
+        if (this.lat == null || this.lon == null) {
+          // Return an empty observable or a default value if lat or lon is null
+          return of(null); // Or use skip() to ignore this emission
+        }
+        return this.cameraService.getClosestCamera(this.lat, this.lon).pipe(
           map(({ camera, distance }) => ({
             maxSpeed: camera.maxSpeed,
             distance: distance
           }))
-        )
-      )
+        );
+      })
     );
+  }
+
+  ngOnDestroy(): void {
+    this.unwatchPosition();
   }
 
   computeDistanceString(distanceInKilometers: number): string {
@@ -49,34 +61,36 @@ export class AppComponent {
       return `${Math.round(distanceInKilometers)} km`;
     } else {
       // If the distance is less than 1 km, convert to meters and round to the nearest 100 meters
-      const distanceInMeters = Math.floor(distanceInKilometers * 1000);      
+      const distanceInMeters = Math.floor(distanceInKilometers * 1000);
       return `${distanceInMeters} m`;
     }
   }
 
-  getCurrentPosition(): Observable<GeolocationCoordinates> {
-    return new Observable<GeolocationCoordinates>(observer => {
-        if (navigator.geolocation) {
-            const watchId = navigator.geolocation.watchPosition(
-                position => {
-                  console.log('Position:', position.coords);
-                  observer.next(position.coords);
-                },
-                error => observer.error(error),
-                { enableHighAccuracy: true,
-                  maximumAge: 0,
-                  timeout: 30000
-                 }
-            );
-
-            // Cleanup when the observable is unsubscribed
-            return () => navigator.geolocation.clearWatch(watchId);
-        } else {
-            observer.error('Geolocation not available');
-            
-            // Even though this code path is an error, we must return a cleanup function.
-            return () => {}; // No-op cleanup function
+  watchPosition(): void {
+    if (navigator.geolocation) {
+      const watchId = navigator.geolocation.watchPosition(
+        position => {
+          console.log('Position:', position.coords);
+          this.lat = position.coords.latitude;
+          this.lon = position.coords.longitude;
+        },
+        error => {
+          console.error('Error:', error);
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 0,
+          timeout: 30000
         }
-    });
-}
+      );
+    }
+  }
+
+  unwatchPosition(): void {
+    if (this.watchId) {
+      navigator.geolocation.clearWatch(this.watchId);
+      this.watchId = null;
+    }
+  }
+
 }
